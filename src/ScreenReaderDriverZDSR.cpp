0d957a8eba84f36d816db08bd734a33190f33399
boz700908 <169002502+boz700908@users.noreply.github.com>
2025-06-24 14:01:44 +0800

    更新 ScreenReaderDriverZDSR.cpp

--- src/ScreenReaderDriverZDSR.cpp
+++ src/ScreenReaderDriverZDSR.cpp
@@ -28,7 +28,7 @@ ScreenReaderDriverZDSR::ScreenReaderDriverZDSR() :
     zdsrGetSpeakState = (ZDSRGetSpeakState)GetProcAddress(controller, "GetSpeakState");
     zdsrSpeak = (ZDSRSpeak)GetProcAddress(controller, "Speak");
     zdsrStopSpeak = (ZDSRStopSpeak)GetProcAddress(controller, "StopSpeak");
-	zdsrInitTTS(0, NULL, false);
+	zdsrInitTTS(1, NULL, false);
   }
 }
 
