3066158a2ff70a671333b20c61ed150596477fe9
boz700908 <3468806165@qq.com>
2026-01-19 22:28:28 +0800

    upgrade boyctrl

--- src/ScreenReaderDriverBOY.cpp
+++ src/ScreenReaderDriverBOY.cpp
@@ -51,9 +51,8 @@ bool ScreenReaderDriverBOY::Braille(const wchar_t *str) {
 }
 
 bool ScreenReaderDriverBOY::Silence() {
-  if (BoyStopSpeak) {
-    BoyStopSpeak();
-    return true;
+      if (BoyStopSpeak) return (BoyStopSpeak() == 0);
+      return false;
   }
 
 bool ScreenReaderDriverBOY::IsActive() {
