9d01f05659bcaddb003a3f4ba3851c370d4aeb5e
boz700908 <3468806165@qq.com>
2026-06-06 12:21:23 +0800

    save local changes

--- src/ScreenReaderDriverZDSR.h
+++ src/ScreenReaderDriverZDSR.h
@@ -12,34 +12,33 @@
 #include <windows.h>
 #include "ScreenReaderDriver.h"
 
-class ScreenReaderDriverZDSR : public ScreenReaderDriver
-{
+class ScreenReaderDriverZDSR : public ScreenReaderDriver {
 public:
-    ScreenReaderDriverZDSR();
-    ~ScreenReaderDriverZDSR() override;
+  ScreenReaderDriverZDSR();
+  ~ScreenReaderDriverZDSR() override;
 
 public:
-    bool Speak(const wchar_t *str, bool interrupt) override;
-    bool Braille(const wchar_t *str) override;
-    bool IsSpeaking() override;
-    bool Silence() override;
-    bool IsActive() override;
-    bool Output(const wchar_t *str, bool interrupt) override;
+  bool Speak(const wchar_t *str, bool interrupt) override;
+  bool Braille(const wchar_t *str) override;
+  bool IsSpeaking() override;
+  bool Silence() override;
+  bool IsActive() override;
+  bool Output(const wchar_t *str, bool interrupt) override;
 
 private:
-    typedef int (WINAPI *InitTTS)(int type, const WCHAR* channelName, BOOL bKeyDownInterrupt);
-    typedef int (WINAPI *GetSpeakState)(void);
-    typedef int (WINAPI *Speak)(const WCHAR* text, BOOL bInterrupt);
-    typedef void (WINAPI *StopSpeak)(void);
-    typedef int (WINAPI *Braille)(const WCHAR* text, BOOL bFlashMessage);
+  typedef int (WINAPI *InitTTS)(int type, const WCHAR* channelName, BOOL bKeyDownInterrupt);
+  typedef int (WINAPI *GetSpeakState)(void);
+  typedef int (WINAPI *Speak)(const WCHAR* text, BOOL bInterrupt);
+  typedef void (WINAPI *StopSpeak)(void);
+  typedef int (WINAPI *Braille)(const WCHAR* text, BOOL bFlashMessage);
 
 private:
-    HINSTANCE controller;
-    InitTTS zdsrInitTTS;
-    GetSpeakState zdsrGetSpeakState;
-    Speak zdsrSpeak;
-    StopSpeak zdsrStopSpeak;
-    Braille zdsrBraille;
+  HINSTANCE controller;
+  InitTTS zdsrInitTTS;
+  GetSpeakState zdsrGetSpeakState;
+  Speak zdsrSpeak;
+  StopSpeak zdsrStopSpeak;
+  Braille zdsrBraille;
 };
 
 #endif // _SCREEN_READER_DRIVER_ZDSR_H_
